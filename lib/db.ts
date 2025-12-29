2182c7011351398b763b9b4df7f7166404600682
Will Blackwell <willblackwell4@gmail.com>
2025-12-29 00:19:07 -0600

    Push latest changes for deployment

--- lib/db.ts
+++ lib/db.ts
@@ -15,11 +15,6 @@ export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
   })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
